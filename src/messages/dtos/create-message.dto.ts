7fdbbb875da7e5166e1ccf61fa628d1d1bcca3d8
Shemil Kaweesha <104987513+dewShemil@users.noreply.github.com>
2023-06-23 13:36:37 +0530

    Removed Comments

--- src/messages/dtos/create-message.dto.ts
+++ src/messages/dtos/create-message.dto.ts
@@ -1,8 +1,5 @@
 import { IsString } from 'class-validator';
 
-// DTO --> Kind of like a model in express js
-// class validator is somewhat like Joi NPM Package
-
 export class CreateMessageDto {
   @IsString() // Checks if the content is a string
   content: string;
